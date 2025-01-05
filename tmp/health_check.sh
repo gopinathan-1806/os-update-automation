ccbbec77016ff49f1cecd7e2b7626d1923ce5e89
gopinathan-1806 <gopinath180695@gmail.com>
2025-01-05 22:23:24 +0530

    Create health_check.sh

---
+++ tmp/health_check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Simulated system health status
+cat <<EOF
+            POD01:           Good
+               sc:           Good
+            proxy:           Good
+               as:           Good
+            swarm:           Good
+               tm:           Good
+      fileshipper:           Good
+             ping:           Good
+          DStore1:           Good
+         Metadata:           Good
+EOF
